dc1adc1421555eb8fdf1642166f6005ceffee688
anom538 <whatech9@gmail.com>
2026-03-27 23:19:33 +0530

    Delete .local directory

--- .local/bin/qs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash 
-
-if pgrep -x quickshell >/dev/null; then
-  pkill -x quickshell
-else
-  uwsm-app -- quickshell >/dev/null 2>&1 &
-fi
--- .local/bin/wb-kill.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-    if pgrep -x waybar > /dev/null; then
-        pkill -x waybar
-        exit 0
-    fi
-    sleep 0.2
-done
